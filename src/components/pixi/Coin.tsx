065d0cf7278ab80d3ebb4d364a0c5692680fe839
haakonjacobsen <haakonjacobsen95@gmail.com>
2023-04-02 15:01:28 +0200

    Add coordinates to coin

--- src/components/pixi/Coin.tsx
+++ src/components/pixi/Coin.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useRef } from 'react';
-import { Sprite } from '@pixi/react';
+import {useEffect, useRef, useState} from 'react';
+import {Sprite, Text} from '@pixi/react';
 import { Rectangle, Sprite as PixiSprite } from 'pixi.js';
+import {TextStyle} from "@pixi/text";
 
 type Props = {
   x: number;
   y: number;
+  xCord: number;
+  yCord: number;
   show: boolean;
   bullet: Rectangle | null;
   onHit: () => void;
@@ -13,12 +16,15 @@ type Props = {
 export default function Coin({
   x,
   y,
+  xCord,
+  yCord,
   show,
   bullet,
   onHit,
 }: Props): JSX.Element {
   const ref = useRef<PixiSprite | null>(null);
 
+  const [showCord, setShowCord] = useState(true);
   useEffect(() => {
     if (!bullet || !show || !ref.current) return;
     if (bullet.intersects(ref.current.getBounds())) {
@@ -29,6 +35,7 @@ export default function Coin({
   if (!show) return <></>;
 
   return (
+    <>
     <Sprite
       image='coin.png'
       width={30}
@@ -38,5 +45,19 @@ export default function Coin({
       y={y}
       ref={ref}
     />
+      {showCord && (
+        <Text
+          text={`(${xCord.toFixed(1)}, ${yCord.toFixed(1)})`}
+          anchor={0.5}
+          x={x}
+          y={y+30}
+          style={new TextStyle({
+            fill: 0x000000,
+            fontSize: 12,
+            fontFamily: 'Handdrawn'
+          })
+          }
+        />)}
+  </>
   );
 }
