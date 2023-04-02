94b88e6ca5852eaad79e3b3f740305fcb3aad1a7
haakonjacobsen <haakonjacobsen95@gmail.com>
2023-04-02 22:26:07 +0200

    Refactor level data

--- src/hooks/useLevel.ts
+++ src/hooks/useLevel.ts
@@ -1,4 +1,17 @@
-type Level = {
+export interface IDescriptionPart {
+  type: 'text' | 'image' | 'lottieAnimation' | 'svgAnimation';
+  content: string;
+}
+export interface ILevelData {
+  title: string;
+  descriptions: IDescriptionPart[];
+}
+
+export interface ILevelDataMap {
+  [key: number]: ILevelData;
+}
+
+export interface Level {
   cellSize: number;
   origoPosition: {
     x: number;
@@ -6,9 +19,20 @@ type Level = {
   };
   enemyPositions: number[][];
   coinPositions: number[][];
-};
-const levels: Level[] = [
+  title: string;
+  descriptions: IDescriptionPart[];
+}
+
+export const levels: Level[] = [
   {
+    title: 'Sinus Funksjon Intro',
+    descriptions: [
+      {type: 'text', content: 'Trig functions like sine and cosine have periodic graphs which we called Sinusoidal Graph, or Sine wave.'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:932/format:webp/0*qtpAmBFyFAV_40YH.png'},
+      {type: 'text', content: 'Every period of sine wave is a whole unit circle:'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:800/0*JSIcq8SNHMnUTkv2.gif'},
+      {type: 'lottieAnimation', content: '../assets/lottie/sine-loader.json'},
+    ],
     cellSize: 80,
     origoPosition: {
       y: 0,
@@ -30,6 +54,14 @@ const levels: Level[] = [
     ],
   },
   {
+    title: 'Amplitude Intro',
+    descriptions: [
+      {type: 'text', content: 'Trig functions like sine and cosine have periodic graphs which we called Sinusoidal Graph, or Sine wave.'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:932/format:webp/0*qtpAmBFyFAV_40YH.png'},
+      {type: 'text', content: 'Every period of sine wave is a whole unit circle:'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:800/0*JSIcq8SNHMnUTkv2.gif'},
+      {type: 'lottieAnimation', content: '../assets/lottie/sine-loader.json'},
+    ],
     cellSize: 50,
     origoPosition: {
       y: 0,
@@ -53,6 +85,14 @@ const levels: Level[] = [
     ],
   },
   {
+    title: 'Level 3',
+    descriptions: [
+      {type: 'text', content: 'Trig functions like sine and cosine have periodic graphs which we called Sinusoidal Graph, or Sine wave.'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:932/format:webp/0*qtpAmBFyFAV_40YH.png'},
+      {type: 'text', content: 'Every period of sine wave is a whole unit circle:'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:800/0*JSIcq8SNHMnUTkv2.gif'},
+      {type: 'lottieAnimation', content: '../assets/lottie/sine-loader.json'},
+    ],
     cellSize: 50,
     origoPosition: {
       y: 0,
@@ -73,9 +113,17 @@ const levels: Level[] = [
       [3.14 * 5.5, 1],
       [3.14 * 6, -1],
       [3.14 * 6.5, -3],
-    ],
+    ]
   },
   {
+    title: 'Level 4',
+    descriptions: [
+      {type: 'text', content: 'Trig functions like sine and cosine have periodic graphs which we called Sinusoidal Graph, or Sine wave.'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:932/format:webp/0*qtpAmBFyFAV_40YH.png'},
+      {type: 'text', content: 'Every period of sine wave is a whole unit circle:'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:800/0*JSIcq8SNHMnUTkv2.gif'},
+      {type: 'lottieAnimation', content: '../assets/lottie/sine-loader.json'},
+    ],
     cellSize: 50,
     origoPosition: {
       y: 0,
@@ -96,9 +144,17 @@ const levels: Level[] = [
       [3.14 * 5.5 /2, 2],
       [3.14 * 6 /2, 0],
       [3.14 * 6.5 /2, -2],
-    ],
+    ]
   },
   {
+    title: 'Level 5',
+    descriptions: [
+      {type: 'text', content: 'Trig functions like sine and cosine have periodic graphs which we called Sinusoidal Graph, or Sine wave.'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:932/format:webp/0*qtpAmBFyFAV_40YH.png'},
+      {type: 'text', content: 'Every period of sine wave is a whole unit circle:'},
+      {type: 'image', content: 'https://miro.medium.com/v2/resize:fit:800/0*JSIcq8SNHMnUTkv2.gif'},
+      {type: 'lottieAnimation', content: '../assets/lottie/sine-loader.json'},
+    ],
     cellSize: 50,
     origoPosition: {
       y: 0,
@@ -106,19 +162,19 @@ const levels: Level[] = [
     },
     enemyPositions: [[3.14 * 0.5 /2, -2]],
     coinPositions: [
-      [3.14 * 1 /2, 0],
-      [3.14 * 1.5 /2, 2],
-      [3.14 * 2 /2, 0],
-      [3.14 * 2.5 /2, -2],
-      [3.14 * 3 /2, 0],
-      [3.14 * 3.5 /2, 2],
-      [3.14 * 4 /2, 0],
-      [3.14 * 4.5 /2, -2],
-      [3.14 * 5 /2, 0],
-      [3.14 * 5.5 /2, 2],
-      [3.14 * 6 /2, 0],
-      [3.14 * 6.5 /2, -2],
-    ],
+      [3.14 * 1 /2 - 1, 0],
+      [3.14 * 1.5 /2 - 1, 2],
+      [3.14 * 2 /2 - 1, 0],
+      [3.14 * 2.5 /2 - 1, -2],
+      [3.14 * 3 /2 - 1, 0],
+      [3.14 * 3.5 /2 - 1, 2],
+      [3.14 * 4 /2 - 1, 0],
+      [3.14 * 4.5 /2 - 1, -2],
+      [3.14 * 5 /2 - 1, 0],
+      [3.14 * 5.5 /2 - 1, 2],
+      [3.14 * 6 /2 - 1, 0],
+      [3.14 * 6.5 /2 - 1, -2],
+    ]
   },
 ];
 
