32f4b23861c45706b35a66e908bbb5cb72a00a61
Andreas Winther Moen <andreas@winthermoen.no>
2023-04-07 18:23:33 +0200

    Set vite build target to esnext

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    target: 'esnext',
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true,
+    },
+  },
 });
