febac7869511afa534a6b44c98993031f4c8fcb8
Andreas Winther Moen <andreas@winthermoen.no>
2023-04-27 20:49:42 +0200

    Fix score calculation

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -135,10 +135,10 @@ export default function useData(): DataContext {
     coins[index] = { ...coins[index], isCollected: true };
     dispatch(setCoins(coins));
 
-    //const chargeScoreMultiplier = (data.chargePower + 1) / 2; // 0.5 to 1 depending on charge power
+    // const chargeScoreMultiplier = (data.chargePower + 1) / 2; // 0.5 to 1 depending on charge power
     const coinsCollectedMultiplier = 1 + data.coinsCollectedThisShot * 0.1; // 1 + 0.1 per coin collected
     const shotMultiplier = 1 / ((data.numAttempts - 1) * 0.2 + 1); // 1st shot: 1, 2nd shot: 0.83, 3rd shot: 0.71, 4th shot: 0.63, etc.
-    const levelScore = SCORE_PER_COIN * coinsCollectedMultiplier * shotMultiplier;
+    const levelScore = SCORE_PER_COIN * coinsCollectedMultiplier * shotMultiplier / 2;
     dispatch(setCurrentScore(data.currentScore + levelScore));
 
     dispatch(setCoinsCollectedThisShot(data.coinsCollectedThisShot + 1));
